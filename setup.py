54b65ba033a22a2c70574d8e4eaee14ebdb67f81
Eric Stein <toba@des.truct.org>
2014-12-10 23:00:45 -0600

    irc hard tag to 10.1 because some people who shall remain nameless removed 'deprecated' features that never had a deprecation warning attached to using them.. thanks for that

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='mediorc',
-      version='0.1.1',
+      version='0.1.2',
       description='Mediorc IRC Bot Helpers Library',
       author='Eric Stein',
       author_email='toba@des.truct.org',
       url='https://github.com/eastein/mediorc/',
       packages=['mediorc', 'mediorc_dns'],
-      install_requires=['dnspython', 'irc']
+      install_requires=['dnspython', 'irc==10.1']
      )
